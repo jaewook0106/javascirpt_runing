eb3182bc159541da0494925e38cef8087c5952ed
edward.jw <edward.jw@kakaocorp.com>
2018-03-29 11:37:18 +0900

    sort 연습 업로드

--- JavaScript/scriptData/sort.js
+++ JavaScript/scriptData/sort.js
@@ -1,6 +1,4 @@
-/**
- * Created by edward on 2018. 3. 26..
- */
+'use strict';
 
 // No 1. sort random array
 
@@ -52,4 +50,138 @@ function sort(list) {
 }
 
 
-console.log(sort(getRandomArray(20)));
+console.log(sort(getRandomArray(50)));
+
+
+
+console.log('--------------------------- 절제선');
+
+// No 2. sort data
+
+const data = [
+  {
+    "name":"Edward",
+    "age":28,
+    "team":"FC Diablo",
+    "ranking":1,
+    "goal": 33,
+    "passSuccessRate": '70.5%'
+  },
+  {
+    "name":"Shawn",
+    "age":30,
+    "team":"FC Company",
+    "ranking":6,
+    "goal": 18,
+    "passSuccessRate": '40.1%'
+  },
+  {
+    "name":"Brad",
+    "age":26,
+    "team":"FC Bbang",
+    "ranking":7,
+    "goal": 14,
+    "passSuccessRate": '69.2%'
+  },
+  {
+    "name":"Merin",
+    "age":31,
+    "team":"FC MerDa",
+    "ranking":5,
+    "goal": 23,
+    "passSuccessRate": '53.8%'
+  },
+  {
+    "name":"Messi",
+    "age":29,
+    "team":"FC Barcelona",
+    "ranking":4,
+    "goal": 27,
+    "passSuccessRate": '52.4%'
+  },
+  {
+    "name":"Ronaldo",
+    "age":29,
+    "team":"FC Madrid",
+    "ranking":3,
+    "goal": 29,
+    "passSuccessRate": '53.2%'
+  },
+  {
+    "name":"Larsson",
+    "age":32,
+    "team":"FC Diablo",
+    "ranking":2,
+    "goal": 31,
+    "passSuccessRate": '68.6%'
+  }
+];
+console.table(data);
+
+// 데이터 복사
+// const realData = dataObj(copyObj,data);
+
+function copyObj(obj) {
+  let copy = {};
+  if (typeof obj === 'object' && obj !== null) {
+    for (let prop in obj) {
+      if (obj.hasOwnProperty(prop)) {
+        copy[prop] = copyObj(obj[prop]);
+      }
+    }
+  } else {
+    copy = obj;
+  }
+  return copy;
+}
+function dataObj(copyObj,data){
+  let dataArr = [];
+  data.map(function(data){
+    let dataList = copyObj(data);
+    if(typeof dataList.passSuccessRate === 'string'){
+      dataList.passSuccessRateNumber = Number(dataList.passSuccessRate.replace('%',''));
+    }
+    dataArr.push(dataList);
+  });
+  return dataArr;
+}
+
+// console.log(realData);
+
+function sortList(type,data){
+  const realData = dataObj(copyObj,data);
+  // const dataArr = [];  // data.map(function(item){
+  //   console.log(item[type]);
+  //
+  // });
+  realData.sort(function(a, b){
+    return a[type] - b[type];
+  });
+  if(type === 'goal' || type === 'passSuccessRateNumber'){
+    return realData.reverse();
+  }else{
+    return realData;
+  }
+}
+
+function dataSort(type,data){
+  if(type === 'age'){
+    return sortList(type,data);
+  }else if(type === 'goal'){
+    return sortList(type,data);
+  }else if(type === 'ranking'){
+    return sortList(type,data);
+  }
+  else if(type === 'passSuccessRateNumber'){
+    return sortList(type,data);
+  }
+
+}
+
+console.log('age 순위',dataSort('age',data));
+console.log('ranking 순위',dataSort('ranking',data));
+console.log('goal 순위',dataSort('goal',data));
+console.log('패스 성공률 순위',dataSort('passSuccessRateNumber',data));
+
+
+console.log('원본 데이터', data);
