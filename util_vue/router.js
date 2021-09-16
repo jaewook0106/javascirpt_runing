f517a8eb21e18be0d5c57b40f9c0441b450146c7
edward.jw <edward.jw@kakaocorp.com>
2021-09-16 18:58:51 +0900

    라우터 수정

--- util_vue/router.js
+++ util_vue/router.js
@@ -1,17 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import store from '@/store'
-s
-
-import MasterHelp from '../views/master/MasterHelp'
 import Login from '../views/auth/Login';
 import LoginFail from '../views/auth/LoginFail';
 import ServiceFail from '../views/auth/ServiceFail';
 import NotFound from '../views/errors/NotFound';
-import SuspensionUse from '../views/auth/SuspensionUse';
-import RegisterReject from '../views/auth/RegisterReject';
-import Outstanding from '../views/auth/Outstanding';
-import {verifyModule} from '../utils/util';
 
 import SessionStorageService from '../plugins/SessionStorageService';
 
@@ -48,26 +41,10 @@ const routes = [
     props: true
   },
   {
-    path: '/register-reject/',
-    name: 'RegisterReject',
-    component: RegisterReject,
-  },
-  {
-    path: '/supension-use/',
-    name: 'SuspensionUse',
-    component: SuspensionUse,
-  },
-  {
-    path: '/outstanding/',
-    name: 'Outstanding',
-    component: Outstanding
-  },
-  {
-    path:'/service/:serviceId',
+    path:'/service',
     name: 'main',
     component: Container,
-    beforeEnter: async (to, from, next) => {
-      await store.dispatch('member/getMyInfo', null);
+    beforeEnter: (to, from, next) => {
       next();
     },
     redirect: { name: 'Home' },
@@ -83,69 +60,10 @@ const routes = [
       {
         path: 'search',
         name: 'Search',
-        component: Search
-      },
-      {
-        path: 'part/:id',
-        name: 'Part',
-        component: Part,
-        props: true,
-        beforeEnter: (to, from, next) => {
-          store.dispatch('part/partStatusCheck', to.params.id);
-          next();
+        component: Search,
+        meta: {
+          height:true,
         },
-        redirect:{ name: 'StatusView' },
-        children:[
-          
-          {
-            path: 'member',
-            name: 'Member',
-            component: Member,
-          },
-          {
-            path: 'setting',
-            name: 'SettingView',
-            component: SettingView,
-            redirect:{ name: 'AlarmSetting' },
-            children: [
-              {
-                path: 'alarm',
-                name: 'AlarmSetting',
-                component: AlarmSetting,
-                meta: {
-                  fixedHeight:true,
-                },
-              },
-              {
-                path: 'info',
-                name: 'DeviceSetting',
-                component: DeviceSetting,
-                meta: {
-                  fixedHeight:true,
-                },
-              },
-              {
-                path: 'doc',
-                name: 'DocumentSetting',
-                component: DocumentSetting,
-                meta: {
-                  fixedHeight:true,
-                },
-              },
-              {
-                path: 'delivery',
-                name: 'DeliverySetting',
-                component: DeliverySetting,
-                meta: {
-                  fixedHeight:true,
-                },
-              },
-              
-            ]
-          },
-
-        ]
-
       }
     ]
   },
@@ -160,28 +78,15 @@ const router = new VueRouter({
 })
 
 router.beforeEach((to, from, next)=>{
-  const isNavPartActive = to.path.indexOf('master') === -1 && to.path.indexOf('part') > -1;
-  store.dispatch('layout/setIsPartSelect', isNavPartActive);
-  store.dispatch('layout/heightFixed', to);
 
-  // 쿼리스트링 처리 및 serviceId파라미터 처리
   // 로그인 처리
-  const {code, accessToken, appUserId, serviceId, randomKey} = to.query;
-  // console.log('to', to)
-  // 사용자 ID저장
-  if (appUserId) {
-    sessionStorageService.setUserId(appUserId);
-  }
+  const {code, accessToken, randomKey} = to.query;
+
   // 토큰 저장
   if (accessToken) {
     sessionStorageService.setToken({access_token: accessToken});
   }
-  // 서비스 ID저장
-  if (serviceId) {
-    sessionStorageService.setServiceId(serviceId);
-    // 루트 페이지에서 serviceId파라미터가 있을 시 home으로 리다이렉트
-    // return;
-  }
+
   // randomKey저장
   if (randomKey) {
     sessionStorageService.setRandomKey(randomKey);
@@ -196,48 +101,13 @@ router.beforeEach((to, from, next)=>{
 
   
   const resultCode = [
-    '3030', '3040', '3080', '3130',
-    '3150', '3160',
+    '3030', '3040',
   ];
  
   if (resultCode.indexOf(code) > -1 && from.name !== 'ServiceFail') {
     next({name: 'ServiceFail', params: {code}});
     return;
   }
-  
-  if (code === '3110' && from.name !== 'SuspensionUse'){
-    next({name:'SuspensionUse'});
-    return;
-  }
-  
-  if (code === '3170' && from.name !== 'RegisterReject'){
-    next({name:'RegisterReject'});
-    return;
-  }
- 
-  if (code === '5000' && from.name !== 'LoginFail'){
-    next({name:'LoginFail', params: {code}});
-    return;
-  }
-
-  if(code === '3070') {
-    next( {name: 'Outstanding'});
-    return;
-  }
-
-  // // 루트 페이지에서 storage에 serviceID가 이미 있는 경우 홈으로 이동
-  // const storageServiceId = sessionStorageService.getServiceId();
-  // if(storageServiceId !== undefined && to.path === '/notfound') {
-  //   next(`/${storageServiceId}/home`);
-  //   return;
-  // }
-
-  // 루트 페이지에서 service_id없을 시 로그인으로 리다이렉트
-  // if(!paramServiceId && to.path !== '/login') {
-  //   alert('serviceId 없음!');
-  //   next('/login');
-  //   return;
-  // }
 
   // redirect가 있을 시 페이지 redirect
   const redirectPath = sessionStorageService.getRedirect();
@@ -245,25 +115,6 @@ router.beforeEach((to, from, next)=>{
     sessionStorageService.clearRedirect();
     next(redirectPath);
     return;
-  }
-
-  // 서비스 ID가 필요한 페이지 처리
-  const isServiceIdRequired = to.matched.some(item => {
-    return item.meta.serviceIdRequired;
-  });
-  const {serviceId : paramServiceId} = to.params;
-
-  // 서비스 ID가 없을 시 로그인으로 리다이렉트
-  if(isServiceIdRequired) {
-    if(paramServiceId) {
-      sessionStorageService.setServiceId(paramServiceId);
-      next();
-      return;
-    } else {
-      alert('serviceId 없음!')
-      next('/login');
-      return;
-    }
   }
 
   next();
